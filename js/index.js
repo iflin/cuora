4fdb86b7431c987b9ca21baa652872b6b04e643a
Lee <jessy1092@gmail.com>
2015-10-24 13:06:03 +0800

    When document ready to load js.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
-// 計數器效果 //
-$({
-		countNum: $('#counter').text()
-}).animate({
-		countNum: 15506
-}, {
-		duration: 4000,
-		easing: 'linear',
-		step: function() {
-				$('#counter').text(Math.floor(this.countNum));
-		},
-		complete: function() {
-				$('#counter').text(this.countNum);
-				// alert('finished');
-		}
+$(document).ready(function ()
+{
+	// 計數器效果 //
+	$({
+			countNum: $('#counter').text()
+	}).animate({
+			countNum: 15506
+	}, {
+			duration: 4000,
+			easing: 'linear',
+			step: function() {
+					$('#counter').text(Math.floor(this.countNum));
+			},
+			complete: function() {
+					$('#counter').text(this.countNum);
+					// alert('finished');
+			}
+	});
 });
